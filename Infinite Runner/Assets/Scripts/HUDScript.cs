e67ccda50864e1c043e8a45dbc365ff982396e1b
n8sk8gr8 <portville99@gmail.com>
2015-07-21 21:23:41 -0500

    Programmatically Create Ground
    Added a simple HUD that shows score.  Programmatically generate ground
    objects. Also added two “garbage collector” one to remove only ground
    objects and the second to trigger the end of the game.

---
+++ Infinite Runner/Assets/Scripts/HUDScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUDScript : MonoBehaviour {
+
+	float playerScore = 0f;
+
+	void Awake()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		playerScore += Time.deltaTime;
+	}
+
+	public void IncreaseScore(int amount)
+	{
+		playerScore += amount;
+	}
+
+	void OnGUI()
+	{
+		GUI.Label (new Rect(10, 10, 100, 30), "Score: " + (int) (playerScore * 100f));
+	}
+}
